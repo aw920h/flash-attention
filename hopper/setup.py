6371e9a626e88628a65b040313f5c599b5e5c135
Shivang <40532945+aw920h@users.noreply.github.com>
2026-05-08 02:25:39 -0700

    Refactor CUDAExtension compile args in setup.py
    Removed modified dynamic_c_macro usage from CUDAExtension compile args, as it seeemed useless/cpoy of existing feature

--- hopper/setup.py
+++ hopper/setup.py
@@ -420,7 +420,6 @@ def nvcc_threads_args():
 # Fallbacks in case metadata is missing
 dynamic_abi_tag = "cp39"
 dynamic_python_requires = ">=3.9"
-dynamic_c_macro = "0x03090000"
 
 try:
     target = os.environ.get("TORCH_TARGET_VERSION")
@@ -435,7 +434,6 @@ try:
     if major >= 2 and minor >= 9:
         dynamic_abi_tag = "cp310"
         dynamic_python_requires = ">=3.10"
-        dynamic_c_macro = "0x030A0000"
 except Exception:
     pass # Sets cp39
 
@@ -650,8 +648,8 @@ if not SKIP_CUDA_BUILD:
             name=f"{PACKAGE_NAME}._C",
             sources=sources,
             extra_compile_args={
-                "cxx":["-O3", "-std=c++17", f"-DPy_LIMITED_API={dynamic_c_macro}"],
-                "nvcc": nvcc_flags +[f"-DPy_LIMITED_API={dynamic_c_macro}"],
+                "cxx":["-O3", "-std=c++17"] + stable_args,
+                "nvcc": nvcc_flags,
             },
             include_dirs=include_dirs,
             py_limited_api=True,
